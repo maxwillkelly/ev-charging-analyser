ac5edd1ea180828b12637092a763e5ffa1f62b7f
Max Kelly <maxwill.kelly@gmail.com>
2021-11-01 14:21:05 +0000

    Adds Configuration and Dynamic Port

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -14,6 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
+
 bootstrap();
