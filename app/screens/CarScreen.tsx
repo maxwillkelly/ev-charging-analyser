53e071ab57188dffc99382d42946ca4a8d1e2824
Max Kelly <maxwill.kelly@gmail.com>
2021-11-30 21:17:13 +0000

    Started adding basic details for CarScreen

--- app/screens/CarScreen.tsx
+++ app/screens/CarScreen.tsx
@@ -1,24 +1,52 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-import { Text, View } from '../components/Themed';
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { Text, View } from "../components/Themed";
+
+const BatteryCard = () => {
+  return null;
+};
+
+const RangeCard = () => {
+  return (
+    <View>
+      <Text style={styles.cardHeading}>Range</Text>
+      <Text style={styles.cardHeading}>350 miles</Text>
+    </View>
+  );
+};
+
+const LockCard = () => {
+  return (
+    <View>
+      <Text style={styles.cardHeading}>Car Unlocked</Text>
+    </View>
+  );
+};
 
 const CarScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Car</Text>
+      <Text style={styles.title}>Andy's Tesla Model X</Text>
+      <BatteryCard />
+      <RangeCard />
+      <LockCard />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+  },
+  cardHeading: {
+    fontSize: 16,
+    fontWeight: "400",
   },
 });
 
