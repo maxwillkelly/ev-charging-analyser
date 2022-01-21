ab445bb06f7e257d423a1731e3ff6657497a4d0f
Max Kelly <maxwill.kelly@gmail.com>
2022-01-21 22:39:05 +0000

    Changes Default Map Location to Dundee

--- app/components/map-screen/Map.tsx
+++ app/components/map-screen/Map.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { StyleSheet, Dimensions } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView from "react-native-maps";
 
 export const Map = () => {
   return (
     <MapView
-      provider={PROVIDER_GOOGLE}
       style={styles.map}
       initialRegion={{
-        latitude: 41.3995345,
-        longitude: 2.1909796,
-        latitudeDelta: 0.003,
-        longitudeDelta: 0.003,
+        latitude: 56.46855061730443,
+        longitude: -2.994651893483153,
+        latitudeDelta: 0.12,
+        longitudeDelta: 0.12,
       }}
       mapType="standard"
     />
