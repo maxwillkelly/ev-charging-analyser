9d50268ed054b343652fbb626e29e3cf2cf6efc0
Max Kelly <maxwill.kelly@gmail.com>
2022-02-03 22:25:35 +0000

    Added Cassandra Configuration

--- server/src/cassandra/cassandra.service.ts
+++ server/src/cassandra/cassandra.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { auth, Client, mapping } from 'cassandra-driver';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class CassandraService {
   client: Client;
   mapper: mapping.Mapper;
 
+  public constructor(private configService: ConfigService) {}
+
   private createClient() {
+    const contactPoint = this.configService.get<string>(
+      'CASSANDRA_CONTACT_POINT',
+    );
+    const localDataCenter = this.configService.get<string>(
+      'CASSANDRA_LOCAL_DATA_CENTER',
+    );
+    const keyspace = this.configService.get<string>('CASSANDRA_KEYSPACE');
+    const username = this.configService.get<string>('CASSANDRA_USER');
+    const password = this.configService.get<string>('CASSANDRA_PASSWORD');
+
+    const authProvider = new auth.PlainTextAuthProvider(username, password);
+
     this.client = new Client({
-      contactPoints: ['0.0.0.0'],
-      keyspace: 'ev-charging-analyser',
-      localDataCenter: 'local',
-      authProvider: new auth.PlainTextAuthProvider('cassandra', 'cassandra'),
+      contactPoints: [contactPoint],
+      keyspace,
+      localDataCenter,
+      authProvider,
     });
   }
 
