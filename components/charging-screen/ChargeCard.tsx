9e5460232fe0bd792b682d02905dbc58637627cf
Max Kelly <maxwill.kelly@gmail.com>
2022-03-14 20:07:38 +0000

    Adds some logic for dynamic charge button

--- components/charging-screen/ChargeCard.tsx
+++ components/charging-screen/ChargeCard.tsx
@@ -1,10 +1,31 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import React from "react";
 import colours from "../../styles/colours";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import fonts from "../../styles/fonts";
+import { ChargeState } from "../../api/dtos/Car.dto";
+import { useCarStore } from "../../stores/useCarStore";
 
 const ChargeCard = () => {
+  const { selectedCar } = useCarStore();
+
+  if (!selectedCar) return null;
+
+  const { state } = selectedCar;
+
+  const getChargeActionString = (chargeState: ChargeState) => {
+    switch (chargeState) {
+      case "CHARGING":
+        return "Stop Charging";
+      case "NOT_CHARGING":
+        return "Charge";
+      default:
+        return "Disabled";
+    }
+  };
+
+  const chargeActionString = getChargeActionString(state);
+
   return (
     <Pressable onPress={() => undefined}>
       <View
@@ -12,47 +33,61 @@ const ChargeCard = () => {
           borderRadius: 6,
           margin: 10,
           padding: 10,
-          backgroundColor: colours.lightestGrey,
+          backgroundColor:
+            state === "CHARGING" ? colours.primary : colours.lightestGrey,
           height: 90,
           width: 190,
           alignItems: "flex-start",
           justifyContent: "center",
         }}
       >
-        <Text style={styles.cardHeading}>Charge</Text>
-        <View style={styles.cardCentred}>
-          <View style={styles.cardCentredVertical}>
-            <MaterialCommunityIcons
-              name="power-plug"
-              size={36}
-              color={colours.secondary}
-            />
+        <Text
+          style={{
+            fontFamily: fonts.semiBold,
+            fontSize: 16,
+            color: state === "CHARGING" ? colours.white : colours.black,
+          }}
+        >
+          {chargeActionString}
+        </Text>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            backgroundColor:
+              state === "CHARGING" ? colours.primary : colours.lightestGrey,
+            alignContent: "center",
+            justifyContent: "center",
+            width: "100%",
+          }}
+        >
+          <View
+            style={{
+              backgroundColor:
+                state === "CHARGING" ? colours.primary : colours.lightestGrey,
+              flexDirection: "column",
+              alignContent: "center",
+              justifyContent: "center",
+            }}
+          >
+            {state === "CHARGING" ? (
+              <MaterialCommunityIcons
+                name="power-plug-off"
+                size={36}
+                color={colours.white}
+              />
+            ) : (
+              <MaterialCommunityIcons
+                name="power-plug"
+                size={36}
+                color={colours.secondary}
+              />
+            )}
           </View>
         </View>
       </View>
     </Pressable>
   );
 };
-
-const styles = StyleSheet.create({
-  cardHeading: {
-    fontFamily: fonts.semiBold,
-    fontSize: 16,
-  },
-  cardCentred: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: colours.lightestGrey,
-    alignContent: "center",
-    justifyContent: "center",
-    width: "100%",
-  },
-  cardCentredVertical: {
-    backgroundColor: colours.lightestGrey,
-    flexDirection: "column",
-    alignContent: "center",
-    justifyContent: "center",
-  },
-});
 
 export default ChargeCard;
