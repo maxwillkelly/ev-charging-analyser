a88d51a1e302223927d01210af4fcbb0217b8a0d
Max Kelly <maxwill.kelly@gmail.com>
2022-03-14 19:24:06 +0000

    Removes unnecessary pressables

--- components/charging-screen/BatteryStatusWidget.tsx
+++ components/charging-screen/BatteryStatusWidget.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import fonts from "../../styles/fonts";
 import colours from "../../styles/colours";
@@ -27,38 +27,36 @@ const BatteryStatusWidget = () => {
   const chargeStateString = getChargeStateString(selectedCar.state);
 
   return (
-    <Pressable onPress={() => undefined}>
+    <View
+      style={{
+        borderRadius: 6,
+        margin: 10,
+        padding: 10,
+        backgroundColor: colours.lightestGrey,
+        height: 90,
+        width: 190,
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+      }}
+    >
+      <Text style={styles.cardHeading}>Status</Text>
       <View
         style={{
-          borderRadius: 6,
-          margin: 10,
-          padding: 10,
-          backgroundColor: colours.lightestGrey,
-          height: 90,
-          width: 190,
-          alignItems: "flex-start",
-          justifyContent: "flex-start",
+          width: "100%",
+          flex: 1,
+          alignItems: "center",
+          flexDirection: "row",
+          justifyContent: "space-around",
         }}
       >
-        <Text style={styles.cardHeading}>Status</Text>
-        <View
-          style={{
-            width: "100%",
-            flex: 1,
-            alignItems: "center",
-            flexDirection: "row",
-            justifyContent: "space-around",
-          }}
-        >
-          <MaterialCommunityIcons
-            name="battery-80"
-            size={30}
-            color={colours.secondary}
-          />
-          <Text style={styles.cardDescription}>{chargeStateString}</Text>
-        </View>
+        <MaterialCommunityIcons
+          name="battery-80"
+          size={30}
+          color={colours.secondary}
+        />
+        <Text style={styles.cardDescription}>{chargeStateString}</Text>
       </View>
-    </Pressable>
+    </View>
   );
 };
 
--- components/charging-screen/ChargingConnection.tsx
+++ components/charging-screen/ChargingConnection.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import fonts from "../../styles/fonts";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
@@ -11,40 +11,38 @@ const ChargingConnection = () => {
   if (!selectedCar) return null;
 
   return (
-    <Pressable onPress={() => undefined}>
+    <View
+      style={{
+        borderRadius: 6,
+        margin: 10,
+        padding: 10,
+        backgroundColor: colours.lightestGrey,
+        height: 90,
+        width: 190,
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+      }}
+    >
+      <Text style={styles.cardHeading}>Connection</Text>
       <View
         style={{
-          borderRadius: 6,
-          margin: 10,
-          padding: 10,
-          backgroundColor: colours.lightestGrey,
-          height: 90,
-          width: 190,
-          alignItems: "flex-start",
-          justifyContent: "flex-start",
+          width: "100%",
+          flex: 1,
+          alignItems: "center",
+          flexDirection: "row",
+          justifyContent: "space-around",
         }}
       >
-        <Text style={styles.cardHeading}>Connection</Text>
-        <View
-          style={{
-            width: "100%",
-            flex: 1,
-            alignItems: "center",
-            flexDirection: "row",
-            justifyContent: "space-around",
-          }}
-        >
-          <MaterialCommunityIcons
-            name="ev-station"
-            size={30}
-            color={colours.secondary}
-          />
-          <Text style={styles.cardDescription}>
-            {selectedCar.isPluggedIn ? "Plugged In" : "Unplugged"}
-          </Text>
-        </View>
+        <MaterialCommunityIcons
+          name="ev-station"
+          size={30}
+          color={colours.secondary}
+        />
+        <Text style={styles.cardDescription}>
+          {selectedCar.isPluggedIn ? "Plugged In" : "Unplugged"}
+        </Text>
       </View>
-    </Pressable>
+    </View>
   );
 };
 
