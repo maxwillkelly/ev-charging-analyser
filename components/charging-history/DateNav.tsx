f84b92d902bc1cc249eb06eb1a716dffb76ac4fe
Max Kelly <maxwill.kelly@gmail.com>
2022-03-16 00:07:31 +0000

    Started working on Charging History

---
+++ components/charging-history/DateNav.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colours from "../../styles/colours";
+import fonts from "../../styles/fonts";
+
+const DateNav = () => {
+  return (
+    <View style={styles.dateNav}>
+      <MaterialCommunityIcons
+        name="chevron-left"
+        size={24}
+        color={colours.secondary}
+        style={{ padding: 8 }}
+      />
+      <Text style={styles.date}>Sun, 21 Nov 2021</Text>
+      <MaterialCommunityIcons
+        name="chevron-right"
+        size={24}
+        color={colours.secondary}
+        style={{ padding: 8 }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  dateNav: {
+    display: "flex",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  date: {
+    fontFamily: fonts.semiBold,
+    fontSize: 16,
+  },
+});
+
+export default DateNav;
--- screens/ChargingHistoryScreen.tsx
+++ screens/ChargingHistoryScreen.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import DateNav from "../components/charging-history/DateNav";
 
 const ChargeHistoryScreen = () => {
   return (
-    <View>
-      <Text>ChargeHistoryScreen</Text>
+    <View style={styles.container}>
+      <DateNav />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
 
 export default ChargeHistoryScreen;
