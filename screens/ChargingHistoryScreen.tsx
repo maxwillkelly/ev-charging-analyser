9cf81c8cb05a50b4df5f2b2c6eab2dfe54010c5b
Max Kelly <maxwill.kelly@gmail.com>
2022-03-16 22:58:10 +0000

    Adds no charging history message

--- screens/ChargingHistoryScreen.tsx
+++ screens/ChargingHistoryScreen.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import ChargeItem from "../components/charging-history/ChargeItem";
-import DateNav from "../components/charging-history/DateNav";
+// import ChargeItem from "../components/charging-history/ChargeItem";
+// import DateNav from "../components/charging-history/DateNav";
+import fonts from "../styles/fonts";
 
 const ChargeHistoryScreen = () => {
   return (
     <View style={styles.container}>
-      <DateNav />
-      <ChargeItem />
+      <Text style={{ fontFamily: fonts.medium, fontSize: 14, margin: 16 }}>
+        No charging history is available at this time
+      </Text>
+      {/* <DateNav />
+      <ChargeItem /> */}
     </View>
   );
 };
