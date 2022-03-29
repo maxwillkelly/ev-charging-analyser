a23f0011b0c83d14fef9b50bdc831b6829de271e
Max Kelly <maxwill.kelly@gmail.com>
2022-03-29 20:30:52 +0100

    Deletes deprecated code

--- screens/root/SettingsScreen.tsx
+++ screens/root/SettingsScreen.tsx
@@ -2,17 +2,15 @@
   Much of this code was inspired by this Marius Reimer's article here: https://reime005.medium.com/simple-settings-list-in-react-native-f37109f9f7c5
 */
 
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import * as React from "react";
 import { SectionList, StyleSheet } from "react-native";
 import SettingsListItem from "../../components/settings/SettingsListItem";
 import SettingsListSectionHeader from "../../components/settings/SettingsListSectionHeader";
-import useLogout from "../../hooks/useLogout";
 import colours from "../../styles/colours";
-import { RootTabScreenProps, SettingsTabParamList } from "../../types";
+import { RootTabScreenProps } from "../../types";
 
 export type Setting = {
-  title: "Type pressures" | "Vehicle information" | "Logout" | "Disconnect vehicle";
+  title: string;
   route: string;
 };
 
@@ -51,8 +49,6 @@ export const settingsData: SettingsGroup[] = [
 ];
 
 const SettingsScreen = ({ navigation }: RootTabScreenProps<"Settings">) => {
-  const logout = useLogout();
-
   const renderItem = (props: {
     item: Setting;
     index: number;
@@ -87,17 +83,5 @@ const SettingsScreen = ({ navigation }: RootTabScreenProps<"Settings">) => {
     />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-});
 
 export default SettingsScreen;
