d58023b69b794a3f1ed1071c6e21e2ce90a378b5
Max Kelly <maxwill.kelly@gmail.com>
2022-04-03 01:01:13 +0100

    Fixes error in configuration with app.config.js

--- app.config.js
+++ app.config.js
@@ -1,8 +1,11 @@
 // const API_TEST_URL = "http://localhost:5000";
 const API_TEST_URL = "https://ev-charging-analyser-api-stag.herokuapp.com";
 
-export default {
-  extra: {
-    apiUrl: process.env.API_URL ?? API_TEST_URL,
-  },
+export default ({ config }) => {
+  return {
+    ...config,
+    extra: {
+      apiUrl: process.env.API_URL ?? API_TEST_URL,
+    },
+  };
 };
