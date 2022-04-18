c1ec416a736a36487c1e4674f370f9f2e1e11597
Max Kelly <maxwill.kelly@gmail.com>
2022-04-18 18:51:53 +0100

    Fixes issue with Google Maps key in deployment

--- app.config.js
+++ app.config.js
@@ -1,11 +1,18 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default ({ config }) => {
   return {
     ...config,
+    android: {
+      ...config.android,
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        },
+      },
+    },
     extra: {
       apiUrl: process.env.API_URL,
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
     },
   };
 };
